9cac839b00f47c09443f4879e44f05c616f14442
Deekshith HM <deekshith.h@lucidspire.com>
2026-05-10 09:04:13 +0530

    Removed mandatory for service type id in account line

--- laundry_management/models/account_move_line.py
+++ laundry_management/models/account_move_line.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    service_type_id = fields.Many2one('laundry.service.type',string="Service Type",required=True)
+    service_type_id = fields.Many2one('laundry.service.type',string="Service Type",required=False)
